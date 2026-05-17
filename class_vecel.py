26dc4eaf0e37d541af9f0451b26b3abcf0744326
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-05-17 11:00:55 +0530

    Add files via upload

---
+++ class_vecel.py
@@ -0,0 +1,7 @@
+class vecel:
+    def __init__(self,max_speed,mileg):
+         self.max_speed=max_speed
+         self.mileg=mileg
+mIx=vecel(240,18)
+print("the max speed of the vecel is  ",mIx.max_speed)
+print("the mileg of the vecel is  ",mIx.mileg)
