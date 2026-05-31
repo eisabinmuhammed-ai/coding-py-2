f9cd3efe601e62d074e3c38d429d9d148c0b697e
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-05-31 11:11:08 +0530

    Add files via upload

---
+++ thiswascomputerprais.py
@@ -0,0 +1,15 @@
+class coputer:
+    def __init__(self):
+        self.__maxprice = 900
+    def sell(self):
+        print("selling price :{}".format(self.__maxprice))
+    def setmaxprice(self,price):
+        self.__maxprice =price
+c=coputer()
+
+c.sell()
+#c.__maxprice(1000)
+c.sell()# Online Python compiler (interpreter) to run Python on
+c.setmaxprice(1000)
+c.sell()# Online P
+# Write Python 3 code in this online editor and run it.
