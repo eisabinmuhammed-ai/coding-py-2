b219f666f1ad8fc5de8e9573bc5dce328b4a45be
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-07-25 12:45:38 +0530

    Add files via upload

---
+++ logingin.py
@@ -0,0 +1,9 @@
+from tkinter import*
+root=Tk()
+root.title('longin')
+root.geometry('400x400')
+frame=Frame(master=root,height=200,width=360,bg='white')
+lbl1=Label(frame,text='full name',bg='black',fg='white',width=12)
+lbl1=Label(frame,text='gmail id',bg='black',fg='white',width=12)
+lbl1=Label(frame,text='password',bg='black',fg='white',width=12)
+namentery
