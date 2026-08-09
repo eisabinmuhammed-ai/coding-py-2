27beb867ba128a1d2e9a57ea8c094769d6fd45f2
eisabinmuhammed-ai <eisabinmuhammed@gmail.com>
2026-08-09 17:16:46 +0530

    Add files via upload

---
+++ denamenasion_Caculater.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image,ImageTk
+root=Tk()
+root.title("Denomiation Conter")
+root.columnconfigure(bg='light blue')
+root.geometry('600x600')
+upload=Image.open("app_image.jpg")
+upload=upload.resize
